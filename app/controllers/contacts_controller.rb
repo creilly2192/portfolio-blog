e5151a742a3671b228cb60fcc8bcdf3098ddc775
Christina Reilly <creilly2192@gmail.com>
2016-04-28 17:13:40 -0500

    fix error

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,16 @@
 class ContactsController < ApplicationController
-  def new
-    @contact = Contact.new
-  end
+	def new
+		@contact = Contact.new
+	end
 
-  def create
-    @contact = Contact.new(params[:contact])
+	def create
+		@contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
       flash.now[:error] = nil
     else
-      flash.now[:error] = "There was a problem. Your message was not sent."
-      render 'new'
-    end  
-  end
+      flash.now[:error] = 'Cannot send message.'
+      render :new
+    end
+	end
 end
